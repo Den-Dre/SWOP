4d5ce06cd098b8ebe4389b84a6a91d162f34f914
Andreas <ahinderyckx@yahoo.co.uk>
2021-03-10 18:53:45 +0100

    Completed JavaDoc of the given domain model.

--- src/domainmodel/HyperLink.java
+++ src/domainmodel/HyperLink.java
@@ -1,18 +1,41 @@
 package domainmodel;
 
+/**
+ * A class to represnt Hyperlinks in the domain layer.
+ */
 public class HyperLink extends ContentSpan {
     private String href;
     private TextSpan textSpan;
 
+    /**
+     * Create a HyperLink with the given parameters.
+     * @param href:
+     *            the href attribute of this HyperLink.
+     * @param text:
+     *            the TextSpan associated with this HyperLink.
+     *
+     */
     public HyperLink(String href, TextSpan text) {
         this.href = href;
         this.textSpan = text;
     }
 
+    /**
+     * Get the href string of this HyperLink.
+     *
+     * @return href:
+     *              the href attribute of this HyperLink.
+     */
     public String getHref() {
         return href;
     }
 
+    /**
+     * Get the {@link TextSpan} associated with this HyperLink.
+     *
+     * @return textSpan:
+     *              the {@link TextSpan} associated with this HyperLink.
+     */
     public TextSpan getTextSpan() {
         return textSpan;
     }
