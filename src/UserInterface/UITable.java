eaf27c13633ea0d0b6ff0fa64f619fd54eae0165
Andreas <ahinderyckx@yahoo.co.uk>
2021-04-12 18:39:48 +0200

    Fixed BookmarksDialogScreen layout.
    Added horizontal offsets to UITable such
    that the width of the cells is set correctly.
    This also solves the issue of the overlapping
    of the 'Name'-UITextField with the adjacent
    UITextInputField in the BookmarksDialogScreen.

--- src/UserInterface/UITable.java
+++ src/UserInterface/UITable.java
@@ -107,11 +107,10 @@ public class UITable extends DocumentCell{
      */
     @Override
     public int getMaxHeight() {
-        int maxheight = 0;
-        for (int height :  rowHeights)
-            maxheight += height;
-        return maxheight +(verticalOffset*grid.get(0).size());
-//        return rowHeights.stream().mapToInt(Integer::intValue).sum();
+        int maxHeight = 0;
+        for (int height : rowHeights)
+            maxHeight += height + verticalOffset;
+        return maxHeight;
     }
 
     /**
@@ -120,11 +119,10 @@ public class UITable extends DocumentCell{
      */
     @Override
     public int getMaxWidth() {
-        int maxwidth = 0;
-        for (int width :  columnWidths)
-            maxwidth += width;
-        return maxwidth;
-//        return columnWidths.stream().mapToInt(Integer::intValue).sum();
+        int maxWidth = 0;
+        for (int width : columnWidths)
+            maxWidth += width + horizontalOffset;
+        return maxWidth;
     }
 
     /**
@@ -175,13 +173,9 @@ public class UITable extends DocumentCell{
                 if (columnWidths.size() <= i)
                     columnWidths.add(width);
                 // If there is, check to see whether this new cell is wider or not
-                else {
-                    if (columnWidths.get(i) < width) {
-                        // If it is wider, replace with newly found (wider) width
-                        columnWidths.remove(i);
-                        columnWidths.add(i, width);
-                    }
-                }
+                else if (columnWidths.get(i) < width)
+                    // If it is wider, replace with newly found (wider) width
+                    columnWidths.set(i, width);
             }
         }
         // Set the width of each cell to the maximum width in its column
@@ -192,9 +186,8 @@ public class UITable extends DocumentCell{
                 cell.setWidth(width);
                 // The x-position needs to be updated too. This is the x-pos of the table + the widths of all the cells to this cell's left
                 int offset = 0;
-                for (int j = 0; j < i; j++) offset += columnWidths.get(j);
+                for (int j = 0; j < i; j++) offset += columnWidths.get(j)+horizontalOffset;
                 cell.setxPos(getxPos()+offset);
-
             }
         }
         // Set the width of the table to the max width of the table
@@ -254,6 +247,8 @@ public class UITable extends DocumentCell{
     }
 
     int verticalOffset = 3;
+
+    int horizontalOffset = 3;
 
     /**
      * An {@link ArrayList} that contains {@link ArrayList}'s
